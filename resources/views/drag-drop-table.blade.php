5631e5597eb376fbc34410bc581a5037058efdb3
sujeet kumar <kumarsujeet5565@gmail.com>
2023-05-24 16:04:30 +0530

    drag-drop-update

--- resources/views/drag-drop-table.blade.php
+++ resources/views/drag-drop-table.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}"> 
  
 
-    <title>Sweet Alert Dialogs</title>
+    <title>Drag-drop-table</title>
     
     <link href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     @livewireStyles
